b42bd2c1f198159ba550f06b4038440667b9ef66
Tusharsingh Baghel <tusharamasingh@gmail.com>
2024-11-09 14:23:11 +0530

    Final with all images and edits

--- frontend/src/components/Eyes_Only.jsx
+++ frontend/src/components/Eyes_Only.jsx
@@ -41,18 +41,11 @@ function EyesOnly() {
                 <Tab.Pane eventKey="Eyes_only-tab-1">
                   <div className="row">
                     <div className="col-lg-8 details order-2 order-lg-1">
-                      <h3>Architecto ut aperiam autem id</h3>
+                      <h3>Vellichor</h3>
                       <p className="fst-italic">
-                        Qui laudantium consequatur laborum sit qui ad sapiente
-                        dila parde sonata raqer a videna mareta paulona marka
+                        (n.) the strange wistfulness of used bookstores, which are somehow infused with the passage of time—filled with thousands of old books you’ll never have time to read.
                       </p>
-                      <p>
-                        Et nobis maiores eius. Voluptatibus ut enim blanditiis
-                        atque harum sint. Laborum eos ipsum ipsa odit magni.
-                        Incidunt hic ut molestiae aut qui. Est repellat minima
-                        eveniet eius et quis magni nihil. Consequatur dolorem
-                        quaerat quos qui similique accusamus nostrum rem vero.
-                      </p>
+                      
                     </div>
                     <div className="col-lg-4 text-center order-1 order-lg-2">
                       <img
