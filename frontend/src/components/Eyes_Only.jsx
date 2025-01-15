65971f88b8c47a867629c24692ed25d35e9b796e
TUSHARSINGH BAGHEL <118206045+TusharsinghBaghel@users.noreply.github.com>
2025-01-15 10:56:30 +0530

    Update Eyes_Only.jsx

--- frontend/src/components/Eyes_Only.jsx
+++ frontend/src/components/Eyes_Only.jsx
@@ -97,7 +97,7 @@ function EyesOnly() {
                     <button
                       className="btn btn-primary btn-golden"
                       onClick={() => navigate("/prose/hindi")}
-                      style={{ width: "400px" , marginLeft: "-560px"}}
+                      style={{ width: "400px" }}
                     >
                       Explore More
                     </button>{" "}
