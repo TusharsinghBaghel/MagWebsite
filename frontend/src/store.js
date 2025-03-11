5b430d83c33f9f438c7c5aab2b36ac5249113bca
TUSHARSINGH BAGHEL <118206045+TusharsinghBaghel@users.noreply.github.com>
2025-03-11 22:59:29 +0530

    Update store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,3 +1,3 @@
 import React from "react";
-// export const BASE_URL = "https://magwebsite-t71p.onrender.com";
-export const BASE_URL = "https://magbackend.wonderfulflower-4d686045.centralindia.azurecontainerapps.io";
+export const BASE_URL = "https://magwebsite-t71p.onrender.com";
+//export const BASE_URL = "https://magbackend.wonderfulflower-4d686045.centralindia.azurecontainerapps.io";
